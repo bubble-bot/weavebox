4ab30b1f0aad59a16ef69a09284b0033856cffd5
twanies <anthonydm@bubblepost.be>
2015-07-21 12:06:30 +0200

    changed commentline

--- weavebox.go
+++ weavebox.go
@@ -1,3 +1,7 @@
+// Weavebox is opinion based minimalistic web framework for making fast and
+// powerfull web application in the Go programming language. It is backed by
+// the fastest and most optimized request router available.
+
 package weavebox
 
 import (
@@ -17,10 +21,6 @@ import (
 var defaultErrorHandler = func(w http.ResponseWriter, r *http.Request, err error) {
 	http.Error(w, err.Error(), http.StatusInternalServerError)
 }
-
-// Weavebox is opinion based minimalistic web framework for making fast and
-// powerfull web application in the Go programming language. It is backed by
-// the fastest and most optimized request router available.
 
 type Weavebox struct {
 	// ErrorHandler is invoked whenever a Handler returns an error
