eab3c8ddd12d354dc2477ad84dfb8e20fe840f59
twanies <anthonydm@bubblepost.be>
2015-07-23 09:26:04 +0200

    updated comments on template.go

--- example/template/main.go
+++ example/template/main.go
@@ -9,15 +9,21 @@ import (
 func main() {
 	app := weavebox.New()
 
-	t := weavebox.NewTemplateEngine()
-	t.SetTemplatesWithLayout("pages/layout.html", "pages/index.html")
+	t := weavebox.NewTemplateEngine("pages")
+	t.SetTemplatesWithLayout("layout.html", "user/index.html")
 	t.Init()
 	app.SetTemplateEngine(t)
 
 	app.Get("/", renderIndex)
+	app.Get("/user", renderUserDetail)
 	app.Serve(3000)
 }
 
 func renderIndex(ctx *weavebox.Context, w http.ResponseWriter, r *http.Request) error {
 	return ctx.Render(w, "pages/index.html", nil)
+}
+
+func renderUserDetail(ctx *weavebox.Context, w http.ResponseWriter, r *http.Request) error {
+	username := "anthony"
+	return ctx.Render(w, "user/index.html", username)
 }
--- template.go
+++ template.go
@@ -4,23 +4,29 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"path"
 	"text/template"
 )
 
+// TemplateEngine provides simple, fast and powerfull rendering of HTML pages.
 type TemplateEngine struct {
+	root            string
 	cache           map[string]*template.Template
-	layout          string
 	templates       []string
 	templWithLayout map[string][]string
 }
 
-func NewTemplateEngine() *TemplateEngine {
+// NewTemplateEngine returns a new TemplateEngine object that will look for
+// templates at the given root.
+func NewTemplateEngine(root string) *TemplateEngine {
 	return &TemplateEngine{
 		cache:           map[string]*template.Template{},
 		templWithLayout: map[string][]string{},
+		root:            root,
 	}
 }
 
+// Render renders the template and satisfies the weavebox.Renderer interface.
 func (t *TemplateEngine) Render(w io.Writer, name string, data interface{}) error {
 	if templ, exist := t.cache[name]; exist {
 		return templ.ExecuteTemplate(w, "_", data)
@@ -28,26 +34,36 @@ func (t *TemplateEngine) Render(w io.Writer, name string, data interface{}) erro
 	return fmt.Errorf("template %s could not be found", name)
 }
 
+// SetTemplates sets single templates that not need to be parsed with a layout
 func (t *TemplateEngine) SetTemplates(templates ...string) {
 	for _, template := range templates {
 		t.templates = append(t.templates, template)
 	}
 }
 
+// SetTemplatesWithLayout sets a layout and parses all given templates with that
+// layout.
+// SetTemplatesWithLayout("layout.html",
+// 		"user/index.html",
+//		"user/list.html",
+//		"user/create.html",
+// )
 func (t *TemplateEngine) SetTemplatesWithLayout(layout string, templates ...string) {
 	t.templWithLayout[layout] = templates
 }
 
+// Init parses all the given singel and layout templates. And stores them in the
+// template cache.
 func (t *TemplateEngine) Init() {
 	for layout, templates := range t.templWithLayout {
-		layout, err := ioutil.ReadFile(layout)
+		layout, err := ioutil.ReadFile(path.Join(t.root, layout))
 		handleErr(err)
 
 		for _, page := range templates {
 			parsedLayout, err := template.New("_").Parse(string(layout))
 			handleErr(err)
 
-			templ, err := ioutil.ReadFile(page)
+			templ, err := ioutil.ReadFile(path.Join(t.root, page))
 			handleErr(err)
 
 			parsedTempl, err := parsedLayout.Parse(string(templ))
@@ -58,7 +74,7 @@ func (t *TemplateEngine) Init() {
 	}
 
 	for _, file := range t.templates {
-		templ, err := ioutil.ReadFile(file)
+		templ, err := ioutil.ReadFile(path.Join(t.root, file))
 		handleErr(err)
 
 		parsedTempl, err := template.New("_").Parse(string(templ))
