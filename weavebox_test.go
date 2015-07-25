edc61247cecada80522bb7d1bbe636afe6c4ff45
twanies <anthonydm@bubblepost.be>
2015-07-25 10:50:27 +0200

    added extra weavebox handler and middleware tests

--- weavebox_test.go
+++ weavebox_test.go
@@ -1,11 +1,15 @@
 package weavebox
 
 import (
+	"bytes"
+	"errors"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
+
+	"golang.org/x/net/context"
 )
 
 var noopHandler = func(ctx *Context, w http.ResponseWriter, r *http.Request) error { return nil }
@@ -47,19 +51,139 @@ func TestSubrouter(t *testing.T) {
 }
 
 func TestStatic(t *testing.T) {
-	t.Error("TODO")
+	w := New()
+	w.Static("/public", "./")
+	code, body := doRequest(t, "GET", "/public/README.md", nil, w)
+	isHTTPStatusOK(t, code)
+	if len(body) == 0 {
+		t.Error("body cannot be empty")
+	}
+	if !strings.Contains(body, "weavebox") {
+		t.Error("expecting body containing string (weavebox)")
+	}
+
+	code, body = doRequest(t, "GET", "/public/nofile", nil, w)
+	if code != http.StatusNotFound {
+		t.Error("expecting status 404 got %d", code)
+	}
+}
+
+func TestContext(t *testing.T) {
+	w := New()
+	w.Get("/", checkContext(t, "m1", "m1"))
+	w.Use(func(ctx *Context, w http.ResponseWriter, r *http.Request) error {
+		ctx.Context = context.WithValue(ctx.Context, "m1", "m1")
+		return nil
+	})
+	code, _ := doRequest(t, "GET", "/", nil, w)
+	isHTTPStatusOK(t, code)
+
+	w.Get("/some", checkContext(t, "m1", "m2"))
+	w.Use(func(ctx *Context, w http.ResponseWriter, r *http.Request) error {
+		ctx.Context = context.WithValue(ctx.Context, "m1", "m2")
+		w.WriteHeader(http.StatusBadRequest)
+		return nil
+	})
+	code, _ = doRequest(t, "GET", "/some", nil, w)
+	if code != http.StatusBadRequest {
+		t.Error("expecting %d, got %d", http.StatusBadRequest, code)
+	}
+}
+
+func checkContext(t *testing.T, key, expect string) Handler {
+	return func(ctx *Context, w http.ResponseWriter, r *http.Request) error {
+		value := ctx.Context.Value(key).(string)
+		if value != expect {
+			t.Errorf("expected %s got %s", expect, value)
+		}
+		return nil
+	}
 }
 
 func TestMiddleware(t *testing.T) {
-	t.Error("TODO")
+	buf := &bytes.Buffer{}
+	w := New()
+	w.Use(func(ctx *Context, w http.ResponseWriter, r *http.Request) error {
+		buf.WriteString("a")
+		return nil
+	})
+	w.Use(func(ctx *Context, w http.ResponseWriter, r *http.Request) error {
+		buf.WriteString("b")
+		return nil
+	})
+	w.Use(func(ctx *Context, w http.ResponseWriter, r *http.Request) error {
+		buf.WriteString("c")
+		return nil
+	})
+	w.Use(func(ctx *Context, w http.ResponseWriter, r *http.Request) error {
+		buf.WriteString("d")
+		return nil
+	})
+	w.Get("/", noopHandler)
+	code, _ := doRequest(t, "GET", "/", nil, w)
+	isHTTPStatusOK(t, code)
+	if buf.String() != "abcd" {
+		t.Error("expecting abcd got %s", buf.String())
+	}
 }
 
 func TestBoxMiddlewareReset(t *testing.T) {
-	t.Error("TODO")
+	buf := &bytes.Buffer{}
+	w := New()
+	w.Use(func(ctx *Context, w http.ResponseWriter, r *http.Request) error {
+		buf.WriteString("a")
+		return nil
+	})
+	w.Use(func(ctx *Context, w http.ResponseWriter, r *http.Request) error {
+		buf.WriteString("b")
+		return nil
+	})
+	sub := w.Subrouter("/sub").Reset()
+	sub.Get("/", noopHandler)
+	code, _ := doRequest(t, "GET", "/sub", nil, w)
+	isHTTPStatusOK(t, code)
+	if buf.String() != "" {
+		t.Error("expecting empty buffer got %s", buf.String())
+	}
+}
+
+func TestBoxMiddlewareInheritsParent(t *testing.T) {
+	buf := &bytes.Buffer{}
+	w := New()
+	w.Use(func(ctx *Context, w http.ResponseWriter, r *http.Request) error {
+		buf.WriteString("a")
+		return nil
+	})
+	w.Use(func(ctx *Context, w http.ResponseWriter, r *http.Request) error {
+		buf.WriteString("b")
+		return nil
+	})
+	sub := w.Subrouter("/sub")
+	sub.Get("/", noopHandler)
+	code, _ := doRequest(t, "GET", "/sub", nil, w)
+	isHTTPStatusOK(t, code)
+	if buf.String() != "ab" {
+		t.Error("expecting ab got %s", buf.String())
+	}
 }
 
 func TestErrorHandler(t *testing.T) {
-	t.Error("TODO")
+	w := New()
+	errorMsg := "oops! something went wrong"
+	w.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err error) {
+		w.WriteHeader(http.StatusInternalServerError)
+		if err.Error() != errorMsg {
+			t.Error("expecting %s, got %s", errorMsg, err.Error())
+		}
+	}
+	w.Use(func(ctx *Context, w http.ResponseWriter, r *http.Request) error {
+		return errors.New(errorMsg)
+	})
+	w.Get("/", noopHandler)
+	code, _ := doRequest(t, "GET", "/", nil, w)
+	if code != http.StatusInternalServerError {
+		t.Error("expecting code 500 got %s", code)
+	}
 }
 
 func TestWeaveboxHandler(t *testing.T) {
